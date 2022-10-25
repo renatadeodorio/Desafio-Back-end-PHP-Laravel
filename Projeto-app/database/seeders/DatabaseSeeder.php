c3c933d5a1685f3b5dc34092851b3d12d7c79241
Renata Deodório <renatadeod@gmail.com>
2022-10-25 10:58:41 -0300

    Ajustes Seeder Database

--- Projeto-app/database/seeders/DatabaseSeeder.php
+++ Projeto-app/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsuariosSeeder::class,
+            CarteirasSeeder::class,
+            TransferenciasSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
