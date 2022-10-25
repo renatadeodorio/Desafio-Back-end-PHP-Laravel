7f2938293630cc0508f267c7ba0cd7abfdf2a2dc
Renata Deodório <renatadeod@gmail.com>
2022-10-25 19:13:44 -0300

    Ajustes nomes das tabelas

---
+++ Projeto-app/database/migrations/2022_10_24_152609_create_transfers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('de_user_id');
+            $table->integer('para_user_id');
+            $table->decimal('balance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
